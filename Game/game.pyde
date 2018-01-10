8c0b43165b87d36c8c730927b1a8b46b7f9bf69f
michaelmat72 <33261504+michaelmat72@users.noreply.github.com>
2018-01-10 16:19:07 -0500

    Added field and changed Robot's draw function (originally display)

--- Game/game.pyde
+++ Game/game.pyde
@@ -1,31 +1,71 @@
 from robot import Robot
 
-redRobot = Robot(100, 100)
-
 def setup():
-    size(640, 480)
+    # REMINDER TO IMPLEMENT SCALE FACTOR IN CASE SOMEONE PLAYS ON A SCREEN LARGER OR SMALLER THAN 1080p (if wanted)
+    global field, red_robot, blue_robot
+
+    fullScreen()
+
+    # Scales field and robots to screen size
+    robot_default_size = (99, 84)
+    scale_factor = displayWidth / 1920.0
+    robot_scaled_size = (robot_default_size[0] * scale_factor, robot_default_size[1] * scale_factor)
+
+    red_robot = Robot(Robot.RED, 100, 200, robot_scaled_size[0], robot_scaled_size[1])
+    blue_robot = Robot(Robot.BLUE,  displayWidth - 100, displayHeight - 100, robot_scaled_size[0], robot_scaled_size[1], PI)
+    field = loadImage("../Assets/Images/Field/Field-(No-Scale-or-Switches)-1920-px-wide.png")
+    print(field.height)
+    field.resize(displayWidth, int(scale_factor * field.height))
 
 def draw():
-    fill(255, 255, 255)
-    rect(0, 0, 640, 480)
-    redRobot.display() # Note form Michael: maybe rename to ".draw()" because it's p standard
+    fill(0, 0, 0)
+    rect(0, 0, displayWidth, displayHeight)
+    
+    # NOTE THAT THIS METHOD OF DRAWING THE FIELD IS TEMPORARY AND OPTIMALLY WILL BE DRAWN USING transform() TO CREATE A PROPER REFERENCE FRAME OF SOME SORT AND TO PREVENT HACKINESS
+    imageMode(CORNERS)
+    image(field, (displayWidth - field.width) / 2, displayHeight - field.height, (displayWidth + field.width) / 2, displayHeight)
+    imageMode(CORNER)
+
+    fill(color(255, 255, 255))
+    textSize(40)
+    text(u"Top-Down FIRST® POWER UP", 10, 10 + textAscent())
+    red_robot.draw()
+    blue_robot.draw()
 
 def keyPressed():
-    if(key == 'w' or key == 'W'):
-        redRobot.is_accel = True
-    elif(key == 's' or key == 'S'):
-        redRobot.is_decel = True
-    elif(key == 'a' or key == 'A'):
-        redRobot.is_turn_l = True
-    elif(key == 'd' or key == 'D'):
-        redRobot.is_turn_r = True
+    lowerKey = str(key).lower()
+    if lowerKey == 'w':
+        red_robot.accel = True
+    elif lowerKey == 's':
+        red_robot.decel = True
+    elif lowerKey == 'a':
+        red_robot.turn_l = True
+    elif lowerKey == 'd':
+        red_robot.turn_r = True
+    elif lowerKey == 'i':
+        blue_robot.accel = True
+    elif lowerKey == 'k':
+        blue_robot.decel = True
+    elif lowerKey == 'j':
+        blue_robot.turn_l = True
+    elif lowerKey == 'l':
+        blue_robot.turn_r = True
 
 def keyReleased():
-    if(key == 'w' or key == 'W'):
-        redRobot.is_accel = False
-    elif(key == 's' or key == 'S'):
-        redRobot.is_decel = False
-    elif(key == 'a' or key == 'A'):
-        redRobot.is_turn_l = False
-    elif(key == 'd' or key == 'D'):
-        redRobot.is_turn_r = False
+    lowerKey = str(key).lower()
+    if lowerKey == 'w':
+        red_robot.accel = False
+    elif lowerKey == 's':
+        red_robot.decel = False
+    elif lowerKey == 'a':
+        red_robot.turn_l = False
+    elif lowerKey == 'd':
+        red_robot.turn_r = False
+    elif lowerKey == 'i':
+        blue_robot.accel = False
+    elif lowerKey == 'k':
+        blue_robot.decel = False
+    elif lowerKey == 'j':
+        blue_robot.turn_l = False
+    elif lowerKey == 'l':
+        blue_robot.turn_r = False
--- Game/robot.py
+++ Game/robot.py
@@ -1,64 +1,74 @@
 class Robot:
     # Internal Usage
-    robot_color = 0x000000
-    
+
     # Constants
-    RWIDTH = 28
-    RHEIGHT = 33
     ACCEL = 0.30
     DECEL = 0.93
-    ANGACCEL = PI/60
-    
+    FRICTION = 0.95
+    ANGACCEL = PI / 60
+    RED = color(237, 28, 36)
+    BLUE = color(1, 145, 255)
+
     # States
-    is_accel = False
-    is_decel = False
-    is_turn_l = False
-    is_turn_r = False
-    
-    
-    def __init__(self, x = 0.0, y = 0.0, vel = 0, theta = 0, is_blue = True):
+    accel = False
+    decel = False
+    turn_l = False
+    turn_r = False
+
+    def __init__(self, color = RED, x = 0.0, y = 0.0, w = 99, h = 84, theta = 0, vel = 0):
+        """Initiates the instance of Robot.
+        Will create a red robot at [0, 0] with a width of 99 and a height of 84 that is unmoving
+        and facing right unless otherwise specified"""
         self.x = x
         self.y = y
+        self.color = color
+        self.w = w
+        self.h = h
         self.vel = 0
         self.theta = theta
-        
-        if is_blue:
-            self.robot_color = color(1, 145, 255)
-        else:
-            self.robot_color = color(237, 28, 36)
-    
+
     def get_corners(self):
-        w = self.RWIDTH
-        h = self.RHEIGHT
+        """Returns corners of a rectangle at an angle."""
+        w = self.w
+        h = self.h
         theta = self.theta
         x = self.x
         y = self.y
-        return [[w * cos(theta + PI / 2) + h * cos(theta) + x, w * sin(theta + PI / 2) + h * sin(theta) + y],
-                [w * cos(theta - PI / 2) + h * cos(theta) + x, w * sin(theta - PI / 2) + h * sin(theta) + y],
-                [w * cos(theta - PI / 2) - h * cos(theta) + x, w * sin(theta - PI / 2) - h * sin(theta) + y],
-                [w * cos(theta + PI / 2) - h * cos(theta) + x, w * sin(theta + PI / 2) - h * sin(theta) + y]]
+        return [[(h / 2) * cos(theta + PI / 2) + (w / 2) * cos(theta) + x, (h / 2) * sin(theta + PI / 2) + (w / 2) * sin(theta) + y],
+                [(h / 2) * cos(theta - PI / 2) + (w / 2) * cos(theta) + x, (h / 2) * sin(theta - PI / 2) + (w / 2) * sin(theta) + y],
+                [(h / 2) * cos(theta - PI / 2) - (w / 2) * cos(theta) + x, (h / 2) * sin(theta - PI / 2) - (w / 2) * sin(theta) + y],
+                [(h / 2) * cos(theta + PI / 2) - (w / 2) * cos(theta) + x, (h / 2) * sin(theta + PI / 2) - (w / 2) * sin(theta) + y]]
     
-    def display(self):
-        fill(self.robot_color)
+    def draw(self):
+        """Draws the instance of Robot"""
+        pushMatrix() # Save the empty transform matrix in the stack so that it can be restored for next Robot instance 
         self.move_robot()
+
+        # Sets drawing options for instance of Robot
+        fill(self.color)
         stroke(0)
-        strokeWeight(1)
+        strokeWeight(2)
+
+        # Puts draw functions (e.g., rect(), ellipse()) into robot's reference frame to make drawing easier
+        translate(self.x, self.y)
+        rotate(self.theta)
         
-        c = self.get_corners()
-        quad(c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1], c[3][0], c[3][1])
-        ellipse(self.x + cos(self.theta) * 25, self.y + sin(self.theta) * 25, 7, 7) # Temporary circle to show which side is front
-        
+        rect(-self.w / 2, - self.h / 2, self.w, self.h, 3)
+        ellipse(25, 0, 11, 11)
+
+        popMatrix() # Restores reference frame to empty transform matrix by popping modified matrix off the stack
+
     def move_robot(self):
-        if self.is_accel:
+        if self.accel:
             self.vel += self.ACCEL
-        if self.is_decel:
+        if self.decel:
             self.vel -= self.ACCEL
-        self.vel *= self.DECEL
-        
-        if self.is_turn_r:
+        self.vel *= self.FRICTION
+
+        if self.turn_r:
             self.theta += self.ANGACCEL
-        if self.is_turn_l:
+        if self.turn_l:
             self.theta -= self.ANGACCEL
-        
-        self.x += self.vel*cos(self.theta)
-        self.y += self.vel*sin(self.theta)
+    
+        self.x += self.vel * cos(self.theta)
+        self.y += self.vel * sin(self.theta)
