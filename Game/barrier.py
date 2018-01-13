51e7a780b3c2c1a424064cdd6d4fea6e50b2e6ae
jtan1 <jtan1@ucvts.tec.nj.us>
2018-01-13 11:29:48 -0500

    Added robot and barrier isColliding functions (untested)

--- Game/barrier.py
+++ Game/barrier.py
@@ -7,3 +7,40 @@ class Barrier:
     
     def draw(self):
         line(x1, y1, x2, y2)
+    
+    def is_colliding(self, barrier):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        barrier.x1 = x3
+        barrier.y1 = y3
+        barrier.x2 = x4
+        barrier.y2 = y4
+        
+        # Avoid division by 0
+        if x1 == x2:
+            x2 += 0.01
+        if x3 == x4:
+            x4 += 0.01
+        
+        # Get equations of both lines (y = mx + b), m is slope, b is y-intercept
+        m1 = (y2 - y1) / (x2 - x1)
+        b1 = y1 - m1*x1
+        m3 = (y4 - y3) / (x4 - x3)
+        b3 = y3 - m3*x3
+    
+        # If lines are parallel, return if they have the same y-intercept
+        if m1 == m3:
+            return b1 == b3
+        
+        # Find intersection point of both lines
+        xInt = (b3 - b1) / (m1 - m3)
+        yInt = m1*xInt + b1
+        
+        # Return true if intersection point is on both line segments
+        onL1x = (x1 <= xInt and xInt <= x2) or (x1 >= xInt and xInt >= x2)
+        onL1y = (y1 <= yInt and yInt <= y2) or (y1 >= yInt and yInt >= y2)
+        onL2x = (x3 <= xInt and xInt <= x4) or (x3 >= xInt and xInt >= x4)
+        onL2y = (y3 <= yInt and yInt <= y4) or (y3 >= yInt and yInt >= y4)
+        return onL1x and onL1y and onL2x and onL2y
--- Game/robot.py
+++ Game/robot.py
@@ -2,7 +2,7 @@ class Robot:
     # Constants
     ACCEL = 0.30
     FRICTION = 0.95
-    ANGACCEL = PI / 60
+    ANGACCEL = PI/60
     RED = color(237, 28, 36)
     BLUE = color(1, 145, 255)
 
@@ -16,13 +16,13 @@ class Robot:
         """Initiates the instance of Robot.
         Will create a red robot at [0, 0] with a width of 99 and a height of 84 that is unmoving
         and facing right unless otherwise specified"""
-        self.pos = PVector(x, y)
+        self.x = x
+        self.y = y
         self.color = color
         self.w = w
         self.h = h
         self.speed = 0
         self.angle = angle
-        self.vel = PVector(0, 0)
 
     def draw(self):
         """Draws the instance of Robot"""
@@ -35,10 +35,10 @@ class Robot:
         strokeWeight(2)
 
         # Puts draw functions (e.g., rect(), ellipse()) into robot's reference frame to make drawing easier
-        translate(self.pos.x, self.pos.y)
+        translate(self.x, self.y)
         rotate(self.angle)
         
-        rect(-self.w / 2, -self.h / 2, self.w, self.h, 3)
+        rect(-self.w/2, -self.h/2, self.w, self.h, 3)
         ellipse(25, 0, 11, 11)
 
         popMatrix() # Restores reference frame to empty transform matrix by popping modified matrix off the stack
@@ -54,8 +54,38 @@ class Robot:
             self.angle += self.ANGACCEL
         if self.turn_l:
             self.angle -= self.ANGACCEL
-    
-        self.vel.x = self.speed * cos(self.angle)
-        self.vel.y = self.speed * sin(self.angle)
         
-        self.pos += self.vel
+        self.x += self.speed * cos(self.angle)
+        self.y += self.speed * sin(self.angle)
+    
+    # Returns corners of robot as list of tuples: [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+    def get_corners(self):
+        """Returns corners of robot as a list of 4 tuples"""
+        w = self.w    
+        h = self.h
+        x = self.x    
+        y = self.y
+        
+        sin_a = sin(self.angle)
+        cos_a = cos(self.angle)
+        
+        dx1 = (w/2) * cos_a - (h/2) * sin_a
+        dy1 = (w/2) * sin_a + (h/2) * cos_a 
+        dx2 = (w/2) * cos_a + (h/2) * sin_a 
+        dy2 = (w/2) * sin_a - (h/2) * cos_a 
+        
+        return [(x + dx1, y + dy1), (x + dx2, y + dy2), (x - dx1, y - dy1), (x - dx2, y - dy2)]
+    
+    def get_lines(self):
+        """Returns edges of robot as a list of 4 barriers"""
+        c = self.get_corners()
+        return [Barrier(c[0][0], c[0][1], c[1][0], c[1][1]),
+                Barrier(c[1][0], c[1][1], c[2][0], c[2][1]),
+                Barrier(c[2][0], c[2][1], c[3][0], c[3][1]),
+                Barrier(c[3][0], c[3][1], c[0][0], c[0][1])]
+    
+    def is_colliding(self, barrier):
+        for robot_edge in self.get_lines():
+            if robot_edge.is_colliding(barrier):
+                return True
+        return False
